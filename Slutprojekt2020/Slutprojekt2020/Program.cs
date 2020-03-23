bdad037d1f40b4c6b50ff205e219df76dcaf43cc
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2020-03-23 11:04:23 +0100

    Update Program.cs

--- Slutprojekt2020/Slutprojekt2020/Program.cs
+++ Slutprojekt2020/Slutprojekt2020/Program.cs
@@ -11,6 +11,11 @@ namespace Slutprojekt2020
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Test");
+			Console.WriteLine("Your goal is to roam around in the house and find a key to escap!");
+			Console.WriteLine("What is your name?");
+			string playerName = Console.ReadLine();
+			Console.WriteLine("Good luck " + playerName );
+			Console.ReadKey();
 		}
 	}
 }
