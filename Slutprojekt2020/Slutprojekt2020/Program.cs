f1ebb0aa8b2614b552916712ae2d0cdf83c79327
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2020-04-14 13:20:55 +0200

    .

--- Slutprojekt2020/Slutprojekt2020/Program.cs
+++ Slutprojekt2020/Slutprojekt2020/Program.cs
@@ -10,26 +10,40 @@ namespace Slutprojekt2020
 	{
 		static void Main(string[] args)
 		{
-			 
+			Items i1 = new Items();
 			Console.WriteLine("Test");
 			Console.WriteLine("Your goal is to roam around in the house and find a key to escape!");
 			Console.WriteLine("However, In every room there will be an obstacle you need to complete before you can find the key");
 			Console.Write("What is your name? ");
 			string playerName = Console.ReadLine();
 			Console.WriteLine("These are your stats ");
-			Player P1 = new Player();//Skapar en ny instan av Player och kör konstruktorn
-			P1.name = playerName;
+			Player p1 = new Player();//Skapar en ny instan av Player och kör konstruktorn
+			p1.name = playerName;
 			Random generator = new Random();
 			int amount = generator.Next(2, 8);
-			Console.WriteLine("Good luck " + P1.name );
-			List<Rooms> ShowRooms = new List<Rooms>(amount);
-			Console.WriteLine(P1.name + " Walks towards then end of the hallway when he suddenly sees the ");
-			ShowRooms.Add(new Rooms());
+			Console.WriteLine("Good luck " + p1.name );
+			Console.WriteLine(p1.name + " Walks towards then end of the hallway when he suddenly sees");
+			Console.WriteLine(" 3 rooms with numbers 1-3 on them, Chose a door to enter");
+			string whatDoor = Console.ReadLine();
+			int roomDecide;
+			bool correctRoomInput = int.TryParse(whatDoor, out roomDecide);
+			while (!correctRoomInput || roomDecide <= 0 || roomDecide >= 4)
+			{
+				Console.WriteLine("Try again, remember to answer in numbers(1,2 or 3)");
+				Console.Write("Choice:");
+				whatDoor = Console.ReadLine();
+				correctRoomInput = int.TryParse(whatDoor, out roomDecide);
+
+			}
+			Console.WriteLine("Funkar???");
+		//	List<Rooms> ShowRooms = new List<Rooms>(amount);
+
+			//	ShowRooms.Add(new Rooms());
 
 			//Console.WriteLine();
 			//for (int i = 1; i < amount + 1; i++)
 			//{
-			
+
 			//}
 			Console.WriteLine(amount);
 			Console.WriteLine("Test");
--- Slutprojekt2020/Slutprojekt2020/Rooms.cs
+++ Slutprojekt2020/Slutprojekt2020/Rooms.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace Slutprojekt2020
 {
-	class Rooms
+	class Rooms : Items
 	{
-		protected int key;
+		
+		private int findkey;
 		protected List<string> roomList = new List<string>() { "Bathroom", "Bedroom", "Kitchen" };
 		protected string roomNames;
 		protected static Random generator = new Random();
 		public Rooms()
 		{
-			 roomNames = roomList[generator.Next(1, roomList.Count())];//Fortsätt här med att lägga in 1 nyckel i ett random rum
-			Console.WriteLine(roomNames);
+			 roomNames = roomList[generator.Next(0, roomList.Count())];//Fortsätt här med att lägga in 1 nyckel i ett random rum
+																	   //Console.WriteLine(roomNames);
+			findkey = generator.Next(1, 4);
 		}
 
+		public int WhatRoom()
+		{
+			Console.WriteLine("You found the " + roomNames );
+			if (findkey == 4)
+			{
+				Console.WriteLine("You found a key!");
+				return findkey;
+				key = key + 1;
+				Console.WriteLine(key);
+			}
+
+			else
+			{
+				Console.WriteLine("You found no key in this room");
+				return generator.Next(5); // Slumpar fram ett tal, beronde på vad talet är kommer man få göra något.
+
+			}
+		}
 		
 	}
 }
