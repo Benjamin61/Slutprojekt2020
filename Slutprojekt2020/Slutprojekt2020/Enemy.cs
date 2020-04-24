976be4ff58589bf4c5da20a2346be28955d9934d
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2020-04-24 16:54:12 +0200

    Fixat API

--- Slutprojekt2020/Slutprojekt2020/Enemy.cs
+++ Slutprojekt2020/Slutprojekt2020/Enemy.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
 
 namespace Slutprojekt2020
 {
 	class Enemy : CharactherCreation
 	{
-		private List<String> whatEnemy = new List<string>() { "Test1", "Test2", "Test3", };//Ska försöka göra en api istället
+		public int enemyGenerator;
+		public  string name1;
+		public  string name2;
+		public  string name3;
+		//{ "Test1", "Test2", "Test3", };//Ska försöka göra en api istället
+		List<String> whatEnemy = new List<string>();
+		public void  getEnemy()
+		{
+			
+			whatEnemy.Add(name1);
+			whatEnemy.Add(name2);
+			whatEnemy.Add(name3);
+			//string allEnemy = string.Join(",", whatEnemy.ToArray());
+			//Console.WriteLine(allEnemy);
+			enemyGenerator = generator.Next(whatEnemy.Count); //Slumpar fram vilken enemy det blir i listan
+			name = whatEnemy[enemyGenerator]; //name blir det namnet sóm är på den plats som enemygenrator slumpade fram från listan
+			Console.WriteLine(name);
+
+
+		}
+
 		public Enemy()//Konstruktor som kommer köras när en ny instans ev enemy skapas
 		{
-			int enemyGenerator = generator.Next(whatEnemy.Count); //Slumpar fram vilken enemy det blir i listan
-			name = whatEnemy[enemyGenerator]; //name blir det namnet så är på den plats som enemygenrator slumpade fram från listan
+
+			
+			
 			if (enemyGenerator == 1)
 			{
 				CharactherStats["hp"] = generator.Next(CharactherStats["hp"], 200);
@@ -37,6 +61,7 @@ namespace Slutprojekt2020
 			Console.WriteLine(name + "used" + attacks[getAttack] + "attack"  );
 			return getAttack;
 		}
+		
 	}
 
 	
--- Slutprojekt2020/Slutprojekt2020/Program.cs
+++ Slutprojekt2020/Slutprojekt2020/Program.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Slutprojekt2020
 {
 	class Program
 	{
 
-
+		
 		static Random generator = new Random();
 		static void Main(string[] args)
 		{
+
+			
+			GetEnemy();
+			
+			
 			int completedAllRooms = 0;
 			Items i1 = new Items();
-			Console.WriteLine("Test");
+			
 			Console.WriteLine("Your goal is to roam around in the house and find a key to escape!");
 			Console.WriteLine("However, In every room there will be an obstacle you need to complete before you can find the key");
 			Console.Write("What is your name? ");
@@ -100,6 +107,61 @@ namespace Slutprojekt2020
 			Console.WriteLine();
 			return 1;
 		}
+		static void GetEnemy()
+		{
+
+			RestClient client = new RestClient("https://swapi.dev/api/");
+			int getNumber = generator.Next(89);
+			int getNumber2 = generator.Next(89);
+			int getNumber3 = generator.Next(89);
+			RestRequest request = new RestRequest("people/" + getNumber + "/");
+			IRestResponse response = client.Get(request);
+			RestRequest request2 = new RestRequest("people/" + getNumber2 + "/");
+			IRestResponse response2 = client.Get(request2);
+			RestRequest request3 = new RestRequest("people/" + getNumber3 + "/");
+			IRestResponse response3 = client.Get(request3);
+
+			
+			Enemy e1 = new Enemy();
+			
+			
+			Enemy people1 = JsonConvert.DeserializeObject<Enemy>(response.Content);
+			e1.name1 = people1.name;
+			Console.WriteLine(e1.name1);
+			Console.WriteLine("här3Enemy1Ovan?");
+			Console.ReadKey();
+			Enemy people2 = JsonConvert.DeserializeObject<Enemy>(response2.Content);
+			e1.name2 = people2.name;
+			Console.WriteLine(e1.name2);
+			Console.WriteLine("här4Enemy2Ovan?");
+			Console.ReadKey();
+			Enemy people3 = JsonConvert.DeserializeObject<Enemy>(response3.Content);
+			e1.name3 = people3.name;
+			Console.WriteLine(e1.name3);
+			Console.WriteLine("här5Enemy3Ovan?");
+			Console.ReadKey();
+
+			e1.getEnemy();
+			Console.WriteLine("här6RandomEnemy");
+			Console.ReadKey();
+			//Lagrar namnet jag fick från api i e1 instansens string name
+
+
+
+
+
+
+
+
+
+
+			Console.ReadKey();
+
+
+
+		}
+		
+		
 		
 	}
 }
